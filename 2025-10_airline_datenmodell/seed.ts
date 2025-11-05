9ff229b8e3a5ed1a86c55584f0507fd537fcee55
floolf234 <PRE210334@spengergasse.at>
2025-11-05 14:52:08 +0100

    Luftfahrt-Datenmodell mit Prisma und Deno

--- 2025-10_airline_datenmodell/seed.ts
+++ 2025-10_airline_datenmodell/seed.ts
@@ -2,6 +2,23 @@ import { PrismaClient } from "model";
 import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
+const ensurePassengers = 1000;
+
+// ensure 20 passengers
+const existingPassengers = await prisma.Passenger.count();
+for (let i = existingPassengers; i < ensurePassengers; i++) {
+    await prisma.Passenger.create({
+        data: {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+        },
+    });
+}
+// ensure 20 plane
+
+// ensure 20 airports
+
 for (let i = 0; i < 10; i++) {
     const fake_airport = faker.airline.airport();
     await prisma.airport.create({
@@ -12,3 +29,5 @@ for (let i = 0; i < 10; i++) {
         },
     });
 }
+
+// ensure 20 flights (depends on airports and planes)
